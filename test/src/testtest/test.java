54baa5a0bc7c0c16fb9130195ed946d8afe4be04
vaikundarajan <vaiku1177@gmail.com>
2022-07-12 14:27:30 +0530

    test file copy

---
+++ test/src/testtest/test.java
@@ -0,0 +1,18 @@
+package testtest;
+
+public class test {
+	public static int x;
+	private void max() {
+		// TODO Auto-generated method stub
+		int z = 2+2;
+		System.out.println(z);
+	}
+	
+	public test(int x) {
+		this.x= x;
+	}
+public static void main(String[] args) {
+	test ob = new test(0);
+	ob.max();
+}
+}
